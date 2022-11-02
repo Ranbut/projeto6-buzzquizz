728a4539e69025064852cdfe8fe0dd0b2e48c9da
tarikochuery <ttarikechuery@gmail.com>
2022-11-02 09:17:07 -0300

    Criação do Quizz: Níveis do Quizz finalizado

---
+++ js/criacao-quizz.js
@@ -0,0 +1,45 @@
+const criacaoNivel = document.querySelector('.criacao-nivel-quiz');
+const criacaoNivelForm = document.querySelector('.container-forms')
+const btnsCriarNivel = criacaoNivelForm.querySelectorAll('ion-icon')
+
+function mostrarEmTela(elemento) {
+  elemento.classList.toggle('escondido')
+}
+
+function construirArrayNivel(e) {
+  e.preventDefault()
+
+  const tituloNivelInputs = criacaoNivel.querySelectorAll('.titulo-nivel')
+  const percentualAcertoInputs = criacaoNivel.querySelectorAll('.percentual-acerto')
+  const urlImagemInputs = criacaoNivel.querySelectorAll('.url-imagem')
+  const descricaoTextAreas = criacaoNivel.querySelectorAll('textarea')
+  let levels = []
+
+  tituloNivelInputs.forEach((input, idx) => {
+    if(input.value === '') return
+    
+    const nivelObj = {
+      title: '',
+      image: '',
+      text: '',
+      minValue: 0
+    }
+
+    nivelObj.title = input.value
+    nivelObj.image = urlImagemInputs[idx].value
+    nivelObj.text = descricaoTextAreas[idx].value
+    nivelObj.minValue = Number(percentualAcertoInputs[idx].value)
+
+    levels = [...levels, nivelObj]
+  })
+  console.log(levels)
+  return levels
+}
+
+function mostrarFormNivel(btn) {
+    const headerForm = btn.parentElement
+    const form = headerForm.nextElementSibling
+    mostrarEmTela(form)
+}
+
+criacaoNivelForm.addEventListener('submit', construirArrayNivel)
