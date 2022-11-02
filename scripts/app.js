5f871aedc8d4da59cc4d3c30cc4fef33a340108d
Ranbut <Ranbut_Beater@protonmail.com>
2022-11-02 13:27:39 -0300

    Adicionado forms de informação e pergunta

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,49 @@
 function criarQuizz(){
 
-    console.log("Selecionado criação de quiz");
+    console.log("Selecionado criação de quizz");
+    console.log("Informações do quizz");
 
-    const menuPrincipal = document.querySelector(".app");
-    const menuDeCriação = document.querySelector(".app-creator")
+    const fecharElemento = document.querySelector(".lista-quizzes");
+    const abrirElement = document.querySelector(".criacao-informacao-quiz")
 
-    menuPrincipal.classList.add("disable");
-    menuDeCriação.classList.toggle("disable");
+    fecharElemento.classList.add("escondido");
+    abrirElement.classList.toggle("escondido");
+}
+
+function criarPerguntas(){
+    /*if (Os forms forem validos){
+
+        //Para a próxima parte
+
+        const fecharElemento = document.querySelector(".criacao-informacao-quiz");
+        const abrirElement = document.querySelector(".criacao-perguntas-quiz")
+        
+        fecharElemento.classList.add("escondido");
+        abrirElement.classList.toggle("escondido");
+    
+        console.log("Perguntas do quizz");
+    }
+    else{
+        alert("Validação incorreta.\nVerifique se os campos estão corretos.")
+        console.log("Validação incorreta!")
+    }*/
+}
+
+function criarNiveis(){
+    /*if (Os forms forem validos){
+
+        //Para a próxima parte
+
+        const fecharElemento = document.querySelector(".criacao-perguntas-quiz");
+        const abrirElement = document.querySelector(".criacao-nivel-quiz")
+    
+        fecharElemento.classList.add("escondido");
+        abrirElement.classList.toggle("escondido");
+
+        console.log("Níveis do quizz");
+    }
+    else{
+        alert("Validação incorreta.\nVerifique se os campos estão corretos.")
+        console.log("Validação incorreta!")
+    }*/
 }
