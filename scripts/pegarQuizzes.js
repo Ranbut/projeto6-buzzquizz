0c20c104668da7a5686b3e7d2b68d205bd15271e
LauraSouzaCastro <laurasouzacastro1@gmail.com>
2022-11-04 21:07:12 -0300

    botoes bonus

--- scripts/pegarQuizzes.js
+++ scripts/pegarQuizzes.js
@@ -45,14 +45,20 @@ function renderizarQuizzes(quizzes) {
 
 function construirHTMLQuiz(quizz) {
   const quizzHTML = `
-    <div
-      onclick="pegarQuiz(${quizz.id})" 
-      class="quiz" 
-      style="background-image: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url('${quizz.image}')"
-    >
-    <p>
-      ${quizz.title}
-    </p>
+    <div class="divRelativa">
+      <div
+        onclick="pegarQuiz(${quizz.id})" 
+        class="quiz" 
+        style="background-image: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url('${quizz.image}')"
+      >
+        <p>
+          ${quizz.title}
+        </p>
+      </div>
+      <div class="opcoesEditaDeleta">
+        <ion-icon name="create-outline" onclick="editarQuiz(${quizz.id})"></ion-icon>
+        <ion-icon name="trash-outline" onclick="deletarQuiz(${quizz.id})"></ion-icon>
+      </div>
     </div>
   `
   return quizzHTML
