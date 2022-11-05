2063de52e2e2e420b75584588781eae5062ff8e4
Vitor Gabriel <59321138+Ranbut@users.noreply.github.com>
2022-11-05 14:09:21 -0300

    Deletar array especifico que contém o ID

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,19 @@
-function btnCriarQuizz(id){
+function btnCriarQuizz(){
+
+    console.log("Selecionado criação de quizz");
+    console.log("Informações do quizz");
+
     const fecharElemento = document.querySelector(".pagina-inicial");
     const abrirElement = document.querySelector(".criacao-informacao-quiz");
 
     fecharElemento.classList.add("escondido");
     abrirElement.classList.remove("escondido");
-    if(id === null){
-        document.querySelector(".titulo-quizz").value = "";
-        document.querySelector(".url-imagem-quizz").value = "";
-        document.querySelector(".quantidade-perguntas-quizz").value = "";
-        document.querySelector(".quantidade-niveis-quizz").value = "";
-    }else{
-        const promessa = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`);
-        promessa.then(colocarInformacoes);
-    }
 }
 
 function deletarQuizz(id){
 
     const QUIZ_KEY = 'quizzes-usuario'
-    const storage = JSON.parse(localStorage.getItem(QUIZ_KEY));
+    let storage = JSON.parse(localStorage.getItem(QUIZ_KEY));
     
     let idValido = false;
     let indexStorage = 0;
@@ -40,10 +35,10 @@ function deletarQuizz(id){
         if(confirm("Você deseja deletar esse quizz?\nNão a como reverter essa não ação.")){
             console.log("Deletando o quizz!");
             const re = axios.delete(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`, config);
-            JSON.parse(localStorage.removeItem(QUIZ_KEY)[indexStorage]);
+            storage.splice(indexStorage, 1);
+            localStorage.setItem(QUIZ_KEY, JSON.stringify(storage));
             re.then(alert("Quizz deletado com sucesso!"));
             pegarQuizzes();
-            window.location.reload();
         }
         else{
             console.log("Ação cancelada!");
@@ -52,43 +47,4 @@ function deletarQuizz(id){
     else{
         alert("O quizz selecionado, não possui nenhum registro local.");
     }
-}
-let keyEdit, idEdit;
-function editarQuizz(id){
-    const QUIZ_KEY = 'quizzes-usuario'
-    const storage = JSON.parse(localStorage.getItem(QUIZ_KEY));
-    
-    let idValido = false;
-    let indexStorage = 0;
-
-    for (let i = 0; i < storage.length; i++) {
-        const idQuizz = storage[i].id;
-
-        if(id === idQuizz){
-            idValido = true; 
-            indexStorage = i;
-            idEdit = id;
-        }
-    }
-
-    if(idValido){
-        const key = storage[indexStorage].secretKey;
-        keyEdit = { headers: { "Secret-Key": key } }
-
-        btnCriarQuizz(id);
-    }
-    else{
-        alert("O quizz selecionado, não possui nenhum registro local.");
-    }
-
-}
-let questionsEdit="", levelsEdit="", quizEdit="";
-function colocarInformacoes(resposta){
-    quizEdit = resposta.data;
-    questionsEdit = quizEdit.questions;
-    levelsEdit = quizEdit.levels;
-    document.querySelector(".titulo-quizz").value = quizEdit.title;
-    document.querySelector(".url-imagem-quizz").value = quizEdit.image;
-    document.querySelector(".quantidade-perguntas-quizz").value = quizEdit.questions.length;
-    document.querySelector(".quantidade-niveis-quizz").value = quizEdit.levels.length;
 }
