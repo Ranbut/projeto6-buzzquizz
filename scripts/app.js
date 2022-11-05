a695a44e37b9a19b0501a4a25a0e218ace774d17
Vitor Gabriel <59321138+Ranbut@users.noreply.github.com>
2022-11-04 21:56:51 -0300

    Adicionado script de deletar quizzes

--- scripts/app.js
+++ scripts/app.js
@@ -9,3 +9,36 @@ function btnCriarQuizz(){
     fecharElemento.classList.add("escondido");
     abrirElement.classList.remove("escondido");
 }
+
+function deletarQuizz(id){
+
+    const QUIZ_KEY = 'quizzes-usuario'
+    const storage = JSON.parse(localStorage.getItem(QUIZ_KEY));
+    
+    let idValido = false;
+
+    for (let i = 0; i < storage.length; i++) {
+        const idQuizz = storage[i].id;
+
+        if(id === idQuizz)
+            idValido = true; 
+    }
+
+    if(idValido){
+        const key = storage.secretKey;
+
+        const config = { headers: { "Secret-Key": key } }
+    
+        if(confirm("Você deseja deletar esse quizz?\nNão a como reverter essa não.")){
+            console.log("Deletando o quizz!");
+                const re = axios.delete(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`, config);
+            re.then(alert("Quizz deletado com sucesso!"));
+        }
+        else{
+            console.log("Ação cancelada!");
+        }
+    }
+    else{
+        alert("O quizz selecionado, não possui nenhum registro local.");
+    }
+}
